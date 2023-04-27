40c8fd0711078f8ce28d585348ef16656c720f6c
vanemoraess <vanessamoraes@ufrj.br>
2023-04-26 21:41:09 -0300

    updating API with POST method

--- women.js
+++ women.js
@@ -1,25 +1,36 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express") //starting Express
+const router = express.Router() //setting up the first part of the route
+const { v4: uuidv4 } = require('uuid');
 
-const app = express()
-const portNumber = 3333
+const app = express() //starting the app
+app.use(express.json())
+const portNumber = 3333 //creating port
 
-function showWoman(request, response){
+//Initial list of women
+const women =[
+
+]
+
+//GET
+function showWomen(request, response){
  response.json(
     [
         {
+            id: "1",
             nome: "Simara Conceição",
             imagem: "https://bit.ly/LJIyOF",
             minibio: "Desenvolvedora e instrutora"
         },
 
         {
+            id: "2",
             nome: "Iana Chan",
             imagem: "https://bit.ly/3JCXBqP",
             minibio: "CEO e fundadora da Programaria"
 
         },
         {
+            id:"3",
             nome: "Luana Pimentel",
             imagem: "https://bit.ly/3FKpFaz",
             minibio: "Staff Senior Software Engineer"
@@ -27,9 +38,25 @@ function showWoman(request, response){
     ]
  )
 }
+
+//POST
+function addWoman(request, response){
+    const newWoman = {
+        id: uuidv4(),
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio
+    }
+
+    women.push(newWoman)
+    response.json(women)
+}
+
+//PORT
 function displayDoor(){
     console.log("Server created and running on port", portNumber )
 }
 
-app.use(router.get("/woman", showWoman))
-app.listen(portNumber, displayDoor)
+app.use(router.get("/women", showWomen))
+app.use(router.post("/women", addWoman))
+app.listen(portNumber, displayDoor) //server listening on port
